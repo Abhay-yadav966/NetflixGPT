e1c2a6369ba1bbdc7db64d4b24cefe69e9450615
Abhay Yadav <yadavabhay966@gmail.com>
2023-12-25 03:22:22 +0530

    Navbar added to Movie info page

--- src/Pages/MovieInfo.jsx
+++ src/Pages/MovieInfo.jsx
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react'
 import { useParams } from 'react-router-dom';
 import {getMovieInfo} from '../services/operations/searchMovieAPI'
 import { useDispatch, useSelector } from 'react-redux';
+import Navbar from '../components/Common/Navbar';
 
 const MovieInfo = () => {
 
@@ -10,10 +11,6 @@ const MovieInfo = () => {
 
     // fetching data from slice
     const { movieInfo} = useSelector((state) => state.search);
-
-    console.log("value of id -->", id);
-    console.log("value of backdrop image ", movieInfo?.backdrop_path)
-    console.log("Movie info", movieInfo);
    
     useEffect(() => {
         dispatch(getMovieInfo(id));
@@ -21,8 +18,12 @@ const MovieInfo = () => {
 
   return (
     <div className='h-screen w-screen bg-no-repeat bg-cover' style={{ backgroundImage : `url(https://image.tmdb.org/t/p/w500/${movieInfo?.backdrop_path})`}} >
-        <div className='w-full h-full bg-gradient-to-b from-black via-black to-black opacity-60' >
-
+        <div className='w-full h-full bg-gradient-to-b from-black via-black to-black opacity-80' >
+            <Navbar/>
+            <div className='w-11/12 max-w-7xl mx-auto h-full' >
+                {/* image */}
+                <img src={`https://image.tmdb.org/t/p/w500/${movieInfo?.poster_path}`} alt="Movie Image" className='w-[300px]' />
+            </div>
         </div>
     </div>
   )
