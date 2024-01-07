6e695b8a76c7deeb229f0fec001789ffe2cebd70
Abhay Yadav <yadavabhay966@gmail.com>
2024-01-07 12:37:15 +0530

    background added to search page

--- src/Pages/GPTSearch.jsx
+++ src/Pages/GPTSearch.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
 import Navbar from '../components/Common/Navbar';
-import Tamplate from '../components/Common/Tamplate';
+
 
 const GPTSearch = () => {
   return (
-    <div>
+    <div className='relative' >
         <Navbar/>
-        <Tamplate/>
+        
+        {/* background image */}
+        <img src="https://assets.nflxext.com/ffe/siteui/vlv3/93da5c27-be66-427c-8b72-5cb39d275279/94eb5ad7-10d8-4cca-bf45-ac52e0a052c0/IN-en-20240226-popsignuptwoweeks-perspective_alpha_website_medium.jpg" alt="background image" 
+          className='bg-cover w-screen'
+        />
+
+        <div className='absolute top-0 w-full border border-red-500' >
+
+        </div>
     </div>
   )
 }
