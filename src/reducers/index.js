2da958fc2d3d6d7d01f15f68529b0c326a101d18
Abhay Yadav <yadavabhay966@gmail.com>
2024-03-16 21:45:38 +0530

    Movie Slice Setup Successfully

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,9 @@
 import { combineReducers } from "@reduxjs/toolkit";
 import authReducer from '../slices/authSlice';
+import movieReducer from '../slices/movieSlice'
 
 
 export const rootReducer = combineReducers({
     auth:authReducer,
+    movie:movieReducer,
 })
---
+++ src/slices/movieSlice.js
@@ -0,0 +1,18 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    nowPlayingMovies:[],
+}
+
+export const movieSlice = createSlice({
+    name:"movie",
+    initialState:initialState,
+    reducers:{
+        setNowPlayingMovies:(state, action) =>{
+            state.nowPlayingMovies = action.payload;
+        }
+    }
+})
+
+export const { setNowPlayingMovies } = movieSlice.action;
+export default movieSlice.reducer;
