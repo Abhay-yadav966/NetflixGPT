3356ea996181510755f3e32cab764a67bd169a6c
Abhay Yadav <yadavabhay966@gmail.com>
2024-04-04 19:21:31 +0530

    Some changes in CORS policy for deployment

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
     cors({
-        origin:"https://netflix-abhay-yadav.vercel.app/",
+        origin:"https://netflix-abhay-yadav.vercel.app",
         credentials:true,
     })
 );
